0a98f38028184b96a67091fc493808bd4a51d506
zhuwenxing <wenxing.zhu@zilliz.com>
2023-04-03 20:02:08 +0800

    Add method to make all content be cached
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- gptcache/processor/pre.py
+++ gptcache/processor/pre.py
@@ -1,2 +1,12 @@
 def last_content(data, **kwargs):
     return data.get("messages")[-1]["content"]
+
+def all_content(data, **kwargs):
+    s = ""
+    messages = data.get("messages")
+    for i, message in enumerate(messages):
+        if i == len(messages) - 1:
+            s += message["content"]
+        else:
+            s += message["content"] + "\n"
+    return s
