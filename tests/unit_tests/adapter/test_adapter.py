8a8564a9260336fcd8a5b815196fcd39b3030b52
SimFG <bang.fu@zilliz.com>
2023-04-05 22:47:57 +0800

    Add the unit test for map data manager and adapter
    Signed-off-by: SimFG <bang.fu@zilliz.com>

--- tests/unit_tests/adapter/test_adapter.py
+++ tests/unit_tests/adapter/test_adapter.py
@@ -31,19 +31,47 @@ def test_adapt():
 
     if os.path.isfile(data_map_path):
         os.remove(data_map_path)
-
     cache.init(pre_embedding_func=pre_embedding)
 
     def report_func(delta_time):
-        assert delta_time > 0.9
+        assert 0.9 < delta_time < 1.1, delta_time
 
-    def add1():
-        res = add_llm(a=1, b=2)
+    def add1(**kwargs):
+        res = add_llm(a=1, b=2, **kwargs)
         assert res == 3, res
 
     time_cal(add1, report_func=report_func)()
 
+    # test cache_skip param
+    def delay_embedding(data, **kwargs):
+        time.sleep(0.5)
+        return data
+    cache.init(pre_embedding_func=pre_embedding, embedding_func=delay_embedding)
+    time_cal(add1, report_func=report_func)(cache_skip=True)
+
     def report_func(delta_time):
-        assert delta_time < 0.2
+        assert delta_time < 0.6, delta_time
 
     time_cal(add1, report_func=report_func)()
+
+    # test cache_enable_func
+    def update_cache_callback(llm_data, update_cache_func):
+        time.sleep(0.5)
+        update_cache_func(str(llm_data))
+        return llm_data
+
+    def disable_cache(*args, **kwargs):
+        return False
+
+    def report_func(delta_time):
+        assert 0.9 < delta_time < 1.1, delta_time
+
+    def add_llm(*args, **kwargs):
+        return adapt(llm_handler, cache_data_convert, update_cache_callback, *args, **kwargs)
+
+    def add2(**kwargs):
+        res = add_llm(a=1, b=2, **kwargs)
+        assert res == 3, res
+
+    cache.init(cache_enable_func=disable_cache, pre_embedding_func=pre_embedding, embedding_func=delay_embedding)
+    time_cal(add2, report_func=report_func)()
---
+++ tests/unit_tests/cache/test_map.py
@@ -0,0 +1,20 @@
+import os
+
+from gptcache.cache.data_manager import MapDataManager
+
+data_map_path = "data_map.txt"
+
+
+def test_map():
+    if os.path.isfile(data_map_path):
+        os.remove(data_map_path)
+
+    data_manager = MapDataManager(data_map_path, 3)
+    a = 'a'
+    for i in range(4):
+        data_manager.save(chr(ord(a) + i), str(i), chr(ord(a) + i))
+    assert len(data_manager.search('a')) == 0
+    question, answer = data_manager.search('b')[0]
+    assert question == 'b', question
+    assert answer == '1', answer
+    data_manager.close()
