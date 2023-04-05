e73f11e7b49332b5a5a6a5fe9bc01afe8f34b0a7
Jael Gu <mengjia.gu@zilliz.com>
2023-04-05 23:46:23 +0800

    Add cohere embedding (#76)
    Signed-off-by: Jael Gu <mengjia.gu@zilliz.com>

--- gptcache/embedding/__init__.py
+++ gptcache/embedding/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ['Towhee', 'OpenAI', 'Huggingface', 'SBERT']
+__all__ = ['Towhee', 'OpenAI', 'Huggingface', 'SBERT', 'Cohere']
 
 from gptcache.utils.lazy_import import LazyImport
 
@@ -6,12 +6,16 @@ towhee = LazyImport('towhee', globals(), 'gptcache.embedding.towhee')
 openai = LazyImport('openai', globals(), 'gptcache.embedding.openai')
 huggingface = LazyImport('huggingface', globals(), 'gptcache.embedding.huggingface')
 sbert = LazyImport('sbert', globals(), 'gptcache.embedding.sbert')
+cohere = LazyImport('cohere', globals(), 'gptcache.embedding.cohere')
 
 
 def Towhee(model="paraphrase-albert-small-v2-onnx"):
     return towhee.Towhee(model)
 
-def OpenAI(model="text-embedding-ada-002", api_key=None, ):
+def Cohere(model="large", api_key=None):
+    return cohere.Cohere(model, api_key)
+
+def OpenAI(model="text-embedding-ada-002", api_key=None):
     return openai.OpenAI(model, api_key)
 
 def Huggingface(model="sentence-transformers/all-mpnet-base-v2"):
