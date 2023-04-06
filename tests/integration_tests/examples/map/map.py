454f62d9c64dfeb5b2efcd75ac4806feab384acd
binbin <83755740+binbinlv@users.noreply.github.com>
2023-04-06 14:54:23 +0800

    Modify CI files (#93)
    Signed-off-by: Binbin Lv <binbin.lv@zilliz.com>

---
+++ tests/integration_tests/examples/map/map.py
@@ -0,0 +1,46 @@
+import os
+
+from gptcache.cache.factory import get_user_data_manager
+from gptcache.adapter import openai
+from gptcache.core import cache, Cache
+
+
+def run():
+    dir_name, _ = os.path.split(os.path.abspath(__file__))
+    bak_cache = Cache()
+    bak_data_file = dir_name + "/data_map_bak.txt"
+    bak_cache.init(data_manager=get_user_data_manager("map",
+                                                 data_path=bak_data_file,
+                                                 max_size=10))
+    data_file = dir_name + "/data_map.txt"
+    cache.init(data_manager=get_user_data_manager("map",
+                                             data_path=data_file,
+                                             max_size=10),
+               next_cache=bak_cache)
+
+    cache.set_openai_key()
+    mock_messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "foo15"}
+    ]
+
+    if not os.path.isfile(bak_data_file):
+        for i in range(10):
+            question = f"foo{i}"
+            answer = f"receiver the foo {i}"
+            cache.data_manager.save(question, answer, cache.embedding_func(question))
+    if not os.path.isfile(data_file):
+        for i in range(10, 20):
+            question = f"foo{i}"
+            answer = f"receiver the foo {i}"
+            bak_cache.data_manager.save(question, answer, bak_cache.embedding_func(question))
+
+    answer = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=mock_messages,
+    )
+    print(answer)
+
+
+if __name__ == '__main__':
+    run()
---
+++ tests/integration_tests/examples/sqlite_faiss_onnx/sqlite_faiss_onnx.py
@@ -0,0 +1,58 @@
+import os
+import time
+
+from gptcache.adapter import openai
+from gptcache.core import cache, Config
+from gptcache.cache.factory import get_data_manager
+from gptcache.similarity_evaluation.simple import SearchDistanceEvaluation
+from gptcache.embedding import Onnx
+
+
+def run():
+    onnx = Onnx()
+
+    sqlite_file = "gptcache.db"
+    faiss_file = "faiss.index"
+    has_data = os.path.isfile(sqlite_file) and os.path.isfile(faiss_file)
+    data_manager = get_data_manager("sqlite", "faiss",
+                                       dimension=onnx.dimension, max_size=2000)
+
+    def log_time_func(func_name, delta_time):
+        print("func `{}` consume time: {:.2f}s".format(func_name, delta_time))
+
+    cache.init(embedding_func=onnx.to_embeddings,
+               data_manager=data_manager,
+               similarity_evaluation=SearchDistanceEvaluation(),
+               config=Config(
+                       log_time_func=log_time_func,
+                   ),
+               )
+
+    if not has_data:
+        question = "what do you think about chatgpt"
+        answer = "chatgpt is a good application"
+        cache.data_manager.save(question, answer, cache.embedding_func(question))
+
+    mock_messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "what do you feel like chatgpt"}
+    ]
+
+    # mock_messages = [
+    #     {"role": "system", "content": "You are a helpful assistant."},
+    #     {"role": "user", "content": "what do you think chatgpt"}
+    # ]
+
+    start_time = time.time()
+    answer = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=mock_messages,
+    )
+    end_time = time.time()
+    print("cache hint time consuming: {:.2f}s".format(end_time - start_time))
+
+    print(answer)
+
+
+if __name__ == '__main__':
+    run()
