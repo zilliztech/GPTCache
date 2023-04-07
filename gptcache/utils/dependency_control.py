a778b01995ad8c1be77794a60ebccc7afc43b75c
Frank Liu <frank@frankzliu.com>
2023-04-07 15:33:06 -0700

    Prompt before install (#143)
    Signed-off-by: Frank Liu <frank.liu@zilliz.com>

--- gptcache/utils/dependency_control.py
+++ gptcache/utils/dependency_control.py
@@ -1,12 +1,14 @@
 import subprocess
 
 
-def prompt_install(package):  # pragma: no cover
+def prompt_install(package: str, warn: bool = False):  # pragma: no cover
     """
     Function used to prompt user to install a package.
     """
+    cmd = f"pip install {package}"
     try:
-        cmd = f"pip install {package}"
+        if warn and input(f"Install {package}? Y/n: ") != "Y":
+            raise ModuleNotFoundError(f"No module named {package}")
         subprocess.check_call(cmd, shell=True)
         print(f"{package} installed successfully!")
     except subprocess.CalledProcessError:
