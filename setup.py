3f548aba08e48bc4e9017a2f1127d29e8e4a6434
SimFG <bang.fu@zilliz.com>
2023-04-10 22:16:35 +0800

    Fix the `cache_skip` param don't take effect
    Signed-off-by: SimFG <bang.fu@zilliz.com>

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def parse_requirements(file_name: str) -> List[str]:
 setuptools.setup(
     name="gptcache",
     packages=find_packages(),
-    version="0.1.6",
+    version="0.1.7",
     author="SimFG",
     author_email="bang.fu@zilliz.com",
     description="GPT Cache, a powerful caching library that can be used to speed up and lower the cost of chat "
