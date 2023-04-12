0b70f2d63b819f66af4116218f2a84bef2f1bfa6
SimFG <bang.fu@zilliz.com>
2023-04-12 22:35:38 +0800

    Update the version to `0.1.9`
    Signed-off-by: SimFG <bang.fu@zilliz.com>

--- setup.py
+++ setup.py
@@ -17,11 +17,11 @@ def parse_requirements(file_name: str) -> List[str]:
 setuptools.setup(
     name="gptcache",
     packages=find_packages(),
-    version="0.1.8",
+    version="0.1.9",
     author="SimFG",
     author_email="bang.fu@zilliz.com",
-    description="GPT Cache, a powerful caching library that can be used to speed up and lower the cost of chat "
-                "applications that rely on the LLM service. GPT Cache works as a memcache for AIGC applications, "
+    description="GPTCache, a powerful caching library that can be used to speed up and lower the cost of chat "
+                "applications that rely on the LLM service. GPTCache works as a memcache for AIGC applications, "
                 "similar to how Redis works for traditional applications.",
     long_description=long_description,
     long_description_content_type="text/markdown",
