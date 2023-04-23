531b446bb7935dd4f52711059f6f7835c03dd683
shiyu22 <shiyu.chen@zilliz.com>
2023-04-23 15:50:28 +0800

    Update sql example (#273)
    Signed-off-by: shiyu22 <shiyu.chen@zilliz.com>

--- examples/data_manager/scalar_store.py
+++ examples/data_manager/scalar_store.py
@@ -1,8 +1,10 @@
+import os
+import numpy as np
+
 from gptcache.adapter import openai
 from gptcache import cache
 from gptcache.manager import get_data_manager, CacheBase, VectorBase
 from gptcache.similarity_evaluation.distance import SearchDistanceEvaluation
-import numpy as np
 
 
 d = 8
@@ -14,15 +16,17 @@ def mock_embeddings(data, **kwargs):
 
 def run():
     scalar_stores = [
-        'sqlite',          # `sql_url` defaults to 'sqlite:///./sqlite.db'
-        'postgresql',      # `sql_url` defaults to 'postgresql+psycopg2://postgres:123456@127.0.0.1:5432/postgres'
-        'mysql',           # `sql_url` defaults to 'mysql+pymysql://root:123456@127.0.0.1:3306/mysql'
-        'mariadb',         # `sql_url` defaults to 'mariadb+pymysql://root:123456@127.0.0.1:3307/mysql'
-        'sqlserver',       # `sql_url` defaults to 'mssql+pyodbc://sa:Strongpsw_123@127.0.0.1:1434/msdb?driver=ODBC+Driver+17+for+SQL+Server'
-        'oracle',          # `sql_url` defaults to 'oracle+cx_oracle://oracle:123456@127.0.0.1:1521/?service_name=helowin&encoding=UTF-8&nencoding=UTF-8'
+        CacheBase('sqlite', sql_url='sqlite:///./sqlite.db'),
+        CacheBase('postgresql', sql_url='postgresql+psycopg2://postgres:123456@127.0.0.1:5432/postgres'),
+        CacheBase('mysql', sql_url='mysql+pymysql://root:123456@127.0.0.1:3306/mysql'),
+        CacheBase('mariadb', sql_url='mariadb+pymysql://root:123456@127.0.0.1:3307/mysql'),
+        CacheBase('sqlserver', sql_url='ssql+pyodbc://sa:Strongpsw_123@127.0.0.1:1434/msdb?driver=ODBC+Driver+17+for+SQL+Server'),
+        CacheBase('oracle', sql_url='oracle+cx_oracle://oracle:123456@127.0.0.1:1521/?service_name=helowin&encoding=UTF-8&nencoding=UTF-8'),
     ]
 
     for scalar_store in scalar_stores:
+        if os.path.exists('faiss.index'):
+            os.remove('faiss.index')
         vector_base = VectorBase('faiss', dimension=d)
         data_manager = get_data_manager(scalar_store, vector_base)
         cache.init(embedding_func=mock_embeddings,
@@ -37,7 +41,15 @@ def run():
                 {'role': 'user', 'content': 'what is chatgpt'}
             ],
         )
-        print(answer)
+        print('answer:', answer)
+
+        answer = openai.ChatCompletion.create(
+            model='gpt-3.5-turbo',
+            messages=[
+                {'role': 'user', 'content': 'what is chatgpt'}
+            ],
+        )
+        print('answer cached:', answer)
 
 
 if __name__ == '__main__':
