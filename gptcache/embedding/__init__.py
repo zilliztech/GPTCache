e1c2ce23cb7e46da781a3c7cd83681d91f4d78c5
SimFG <bang.fu@zilliz.com>
2023-04-25 18:02:55 +0800

    Improve the code format and object store (#287)
    Signed-off-by: SimFG <bang.fu@zilliz.com>

--- gptcache/embedding/__init__.py
+++ gptcache/embedding/__init__.py
@@ -13,6 +13,7 @@ data2vec = LazyImport("data2vec", globals(), "gptcache.embedding.data2vec")
 timm = LazyImport("timm", globals(), "gptcache.embedding.timm")
 vit = LazyImport("vit", globals(), "gptcache.embedding.vit")
 
+
 def Cohere(model="large", api_key=None):
     return cohere.Cohere(model, api_key)
 
@@ -43,6 +44,7 @@ def Data2VecAudio(model="facebook/data2vec-audio-base-960h"):
 
 def Timm(model="resnet50", device="default"):
     return timm.Timm(model, device)
+
 
 def ViT(model="google/vit-base-patch16-384"):
     return vit.ViT(model)
