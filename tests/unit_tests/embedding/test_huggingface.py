c99c0854801ae0a27c31b132b1aa1a8332355918
wxywb <xy.wang@zilliz.com>
2023-05-09 14:54:13 +0800

    Use self-hosted tokenizer and update summarization context.
    Signed-off-by: wxywb <xy.wang@zilliz.com>

--- tests/unit_tests/embedding/test_huggingface.py
+++ tests/unit_tests/embedding/test_huggingface.py
@@ -3,10 +3,10 @@ from gptcache.adapter.api import _get_model
 
 
 def test_huggingface():
-    t = Huggingface("sentence-transformers/paraphrase-albert-small-v2")
+    t = Huggingface("distilbert-base-uncased")
     data = t.to_embeddings("foo")
     assert len(data) == t.dimension, f"{len(data)}, {t.dimension}"
 
-    t = _get_model(model_src="huggingface", model_config={"model": "sentence-transformers/paraphrase-albert-small-v2"})
+    t = _get_model(model_src="huggingface", model_config={"model": "distilbert-base-uncased"})
     data = t.to_embeddings("foo")
     assert len(data) == t.dimension, f"{len(data)}, {t.dimension}"
