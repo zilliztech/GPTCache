21b725d15eec3a03ad7fca5b2b73770ece84c515
SimFG <bang.fu@zilliz.com>
2023-05-11 22:24:44 +0800

    Improve the api for the `import_ruamel` (#336)
    Signed-off-by: SimFG <bang.fu@zilliz.com>
    Signed-off-by: feimeng <13263397018@163.com>

--- examples/adapter/api.py
+++ examples/adapter/api.py
@@ -1,10 +1,7 @@
-from gptcache.adapter.api import put, get
-from gptcache.manager import CacheBase, VectorBase, get_data_manager
-from gptcache.processor.pre import get_prompt
-from gptcache.processor.post import nop
 from gptcache import cache, Config, Cache
-from gptcache.embedding import Onnx as EmbeddingOnnx
-from gptcache.similarity_evaluation import SearchDistanceEvaluation
+from gptcache.adapter.api import put, get, init_similar_cache
+from gptcache.processor.post import nop
+from gptcache.processor.pre import get_prompt
 
 
 def run_basic():
@@ -15,21 +12,11 @@ def run_basic():
 
 
 def run_similar_match():
-    embedding_onnx = EmbeddingOnnx()
-    cache_base = CacheBase("sqlite")
-    vector_base = VectorBase("faiss", dimension=embedding_onnx.dimension, top_k=10)
-    data_manager = get_data_manager(cache_base, vector_base)
-
-    evaluation = SearchDistanceEvaluation()
     inner_cache = Cache()
-    inner_cache.init(
-        pre_embedding_func=get_prompt,
-        embedding_func=embedding_onnx.to_embeddings,
-        data_manager=data_manager,
-        similarity_evaluation=evaluation,
-        post_process_messages_func=nop,
-        config=Config(similarity_threshold=0),
+    init_similar_cache(
+        cache_obj=inner_cache, post_func=nop, config=Config(similarity_threshold=0)
     )
+
     put("hello1", "foo1", cache_obj=inner_cache)
     put("hello2", "foo2", cache_obj=inner_cache)
     put("hello3", "foo3", cache_obj=inner_cache)
