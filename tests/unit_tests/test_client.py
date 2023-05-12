ca7f2f8f33ee22961989248d7f7a5d21956226be
KY HU <kaiyuan.hu@zilliz.com>
2023-05-12 18:20:17 +0800

    Add gptcache client (#338)
    Signed-off-by: Kaiyuan Hu <kaiyuan.hu@zilliz.com>

---
+++ tests/unit_tests/test_client.py
@@ -0,0 +1,17 @@
+from gptcache import Client
+from unittest.mock import patch, Mock
+
+def test_client():
+    client = Client()
+    with patch("httpx.AsyncClient.put") as mock_response:
+        mock_response.return_value = Mock(status_code=200)
+        status_code = client.put("Hi", "Hi back")
+        assert status_code == 200
+    
+    with patch("httpx.AsyncClient.get") as mock_response:
+        m = Mock()
+        attrs = {"json.return_value": "Hi back"}
+        m.configure_mock(**attrs)
+        mock_response.return_value = m
+        ans = client.get("Hi")
+        assert ans == "Hi back"
