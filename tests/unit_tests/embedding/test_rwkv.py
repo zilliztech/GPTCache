b507a99afbe94e3537bd2aeb4fb8fae097372a53
wxywb <xy.wang@zilliz.com>
2023-05-18 22:07:37 +0800

    Add rwkv model for embedding. (#363)
    Signed-off-by: wxywb <xy.wang@zilliz.com>

---
+++ tests/unit_tests/embedding/test_rwkv.py
@@ -0,0 +1,14 @@
+import pytest
+from gptcache.embedding import Rwkv
+from gptcache.adapter.api import _get_model
+
+@pytest.mark.tags("L2")
+def test_rwkv():
+    t = Rwkv("sgugger/rwkv-430M-pile")
+    data = t.to_embeddings("foo")
+    assert len(data) == t.dimension, f"{len(data)}, {t.dimension}"
+
+    t = _get_model(model_src="rwkv", model_config={"model": "sgugger/rwkv-430M-pile"})
+    data = t.to_embeddings("foo")
+    assert len(data) == t.dimension, f"{len(data)}, {t.dimension}"
+
