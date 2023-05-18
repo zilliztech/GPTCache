fa1c260aa4effcc63314d0c74cfa897eaeaf6378
vax521 <13263397018@163.com>
2023-05-18 22:56:05 +0800

    Resolving test dependency conflict issues.
    Signed-off-by: vax521 <13263397018@163.com>

--- tests/unit_tests/embedding/test_paddlenlp.py
+++ tests/unit_tests/embedding/test_paddlenlp.py
@@ -1,13 +1,13 @@
 from gptcache.embedding import PaddleNLP
 from gptcache.adapter.api import _get_model
-
+from gptcache.utils.dependency_control import prompt_install
 
 def test_paddlenlp():
     t = PaddleNLP("ernie-3.0-nano-zh")
     dimension = t.dimension
     data = t.to_embeddings("中国")
     assert len(data) == dimension, f"{len(data)}, {t.dimension}"
-
+    prompt_install("protobuf==4.23.1")
     t = _get_model(model_src="paddlenlp", model_config={"model": "ernie-3.0-nano-zh"})
     dimension = t.dimension
     data = t.to_embeddings("中国")
