6a1295fb0c3342edf1f0cc2a3b71ef2aa1d94a34
vax521 <13263397018@163.com>
2023-05-18 10:54:17 +0800

    use small model in test case
    Signed-off-by: vax521 <13263397018@163.com>

--- tests/unit_tests/embedding/test_paddlenlp.py
+++ tests/unit_tests/embedding/test_paddlenlp.py
@@ -3,12 +3,12 @@ from gptcache.adapter.api import _get_model
 
 
 def test_paddlenlp():
-    t = PaddleNLP("ernie-3.0-medium-zh")
+    t = PaddleNLP("ernie-3.0-nano-zh")
     dimension = t.dimension
     data = t.to_embeddings("中国")
     assert len(data) == dimension, f"{len(data)}, {t.dimension}"
 
-    t = _get_model(model_src="paddlenlp", model_config={"model": "ernie-3.0-medium-zh"})
+    t = _get_model(model_src="paddlenlp", model_config={"model": "ernie-3.0-nano-zh"})
     dimension = t.dimension
     data = t.to_embeddings("中国")
     assert len(data) == dimension, f"{len(data)}, {t.dimension}"
