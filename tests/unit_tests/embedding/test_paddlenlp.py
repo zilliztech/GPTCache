40345bd7c043416053d007be764bf05ab2469630
feimeng <13263397018@163.com>
2023-05-19 07:16:11 +0000

    solve test issues
    Signed-off-by: feimeng <13263397018@163.com>

--- tests/unit_tests/embedding/test_paddlenlp.py
+++ tests/unit_tests/embedding/test_paddlenlp.py
@@ -1,13 +1,16 @@
+import subprocess
 from gptcache.embedding import PaddleNLP
 from gptcache.adapter.api import _get_model
-from gptcache.utils.dependency_control import prompt_install
+
 
 def test_paddlenlp():
+    subprocess.check_call("export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python;", shell=True)
+    
     t = PaddleNLP("ernie-3.0-nano-zh")
     dimension = t.dimension
     data = t.to_embeddings("中国")
     assert len(data) == dimension, f"{len(data)}, {t.dimension}"
-    prompt_install("protobuf==4.23.1")
+  
     t = _get_model(model_src="paddlenlp", model_config={"model": "ernie-3.0-nano-zh"})
     dimension = t.dimension
     data = t.to_embeddings("中国")
