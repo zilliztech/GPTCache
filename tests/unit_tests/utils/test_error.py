5af3af23c27cd6f636cf7c722936676e5b3e4fc3
SimFG <bang.fu@zilliz.com>
2023-06-09 11:28:40 +0800

    Raise the same type's error for the openai (#421)
    Signed-off-by: SimFG <bang.fu@zilliz.com>

--- tests/unit_tests/utils/test_error.py
+++ tests/unit_tests/utils/test_error.py
@@ -15,3 +15,23 @@ def test_error_type():
 
     param_error = ParamError("unittest")
     assert issubclass(type(param_error), CacheError)
+
+
+def test_wrap():
+    import openai
+
+    from gptcache.utils.error import wrap_error
+
+    def raise_error():
+        try:
+            raise openai.error.OpenAIError(message="test")
+        except openai.error.OpenAIError as e:
+            raise wrap_error(e)
+
+    is_exception = False
+    try:
+        raise_error()
+    except openai.error.OpenAIError as e:
+        is_exception = True
+
+    assert is_exception
