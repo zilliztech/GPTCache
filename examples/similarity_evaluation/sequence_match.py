cfe2eef9e357af2760676550a7a3abf03f9258b4
ChengZi <chen.zhang@zilliz.com>
2023-06-15 17:04:34 +0800

    Fix sequence match example (#431)
    Signed-off-by: ChengZi <chen.zhang@zilliz.com>

--- examples/similarity_evaluation/sequence_match.py
+++ examples/similarity_evaluation/sequence_match.py
@@ -9,15 +9,14 @@ from gptcache import Config
 
 def run():
     onnx = Onnx()
-    config=Config(skip_list=['system', 'assistant'])
 
     vector_base = VectorBase('faiss', dimension=onnx.dimension)
     data_manager = get_data_manager('sqlite', vector_base)
 
     cache.init(embedding_func=onnx.to_embeddings,
-               pre_embedding_func = concat_all_queries,
+               pre_embedding_func=concat_all_queries,
                data_manager=data_manager,
-               similarity_evaluation=SequenceMatchEvaluation([0.1, 0.2, 0.7], 'onnx'), 
+               similarity_evaluation=SequenceMatchEvaluation([0.1, 0.2, 0.7], 'onnx'),
                config=Config(context_len=3, skip_list=['system', 'assistant'])
                )
     cache.set_openai_key()
@@ -29,7 +28,7 @@ def run():
             {'role': 'user', 'content': 'query1'},
             {'role': 'assistant', 'content': 'answer1'},
             {'role': 'user', 'content': 'query2'},
-            {'role': 'assistant', 'content': 'answer2'}
+            {'role': 'assistant', 'content': 'answer2'},
             {'role': 'user', 'content': 'query3'},
             {'role': 'assistant', 'content': 'answer3'}
         ]
