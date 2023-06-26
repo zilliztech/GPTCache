6772b070f0c48ec61dd6fd6b3e252d68e43a8e9a
Bryan <wybryan@hotmail.com>
2023-06-27 02:35:38 +0800

    implements cache health check

---
+++ tests/integration_tests/test_sqlite_milvus_sbert.py
@@ -0,0 +1,189 @@
+import os
+import shutil
+import pytest
+
+from base.client_base import Base
+from common import common_func as cf
+from gptcache import cache, Config
+from gptcache.adapter import openai
+from gptcache.embedding import Onnx, SBERT
+from gptcache.manager import get_data_manager, VectorBase
+from gptcache.similarity_evaluation.distance import SearchDistanceEvaluation
+from utils.util_log import test_log as log
+
+
+def get_text_response(response):
+    if response is None:
+        return ""
+    collated_response = [
+        chunk["choices"][0]["delta"].get("content", "")
+        for chunk in response
+    ]
+    return "".join(collated_response)
+
+
+class TestSqliteMilvus(Base):
+
+    """
+    ******************************************************************
+    #  The followings are general cases
+    ******************************************************************
+    """
+
+    @pytest.mark.tags("L1")
+    def test_hit_cache_out_of_sync(self):
+        """
+        target: test hit the cache function
+        method: keep default similarity_threshold
+        expected: hit successfully but return wrong reponse to user
+        """
+        if os.path.isfile("./sqlite.db"):
+            os.remove("./sqlite.db")
+        if os.path.isdir('./milvus_data'):
+            shutil.rmtree('./milvus_data')
+
+        onnx = SBERT("/home/bryan/Projects/Github/PayPal/ModelZoo/transformers/all-MiniLM-L6-v2")
+        vector_base = VectorBase("milvus", dimension=onnx.dimension, local_mode=True)
+        data_manager = get_data_manager("sqlite", vector_base, max_size=2000)
+        cache.init(
+            embedding_func=onnx.to_embeddings,
+            data_manager=data_manager,
+            similarity_evaluation=SearchDistanceEvaluation(),
+            config=Config(
+                log_time_func=cf.log_time_func,
+                enable_token_counter=False,
+            ),
+        )
+
+        question = [
+            "what is apple?",
+            "what is intel?",
+            "what is openai?",
+
+        ]
+        answer = [
+            "apple",
+            "intel",
+            "openai"
+        ]
+        for q, a in zip(question, answer):
+            cache.data_manager.save(q, a, cache.embedding_func(q))
+
+        # let's simulate cache out-of-sync
+        # situation.
+        touble_query = "what is google?"
+        cache.data_manager.v.update_embeddings(1, cache.embedding_func(touble_query))
+
+        is_exception = False
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": touble_query},
+            ],
+            search_only=True,
+            stream=True,
+        )
+        try:
+            assert answer[1] == get_text_response(response), "Incorrect response returned to user..."
+        except Exception as e:
+            log.info(e)
+            is_exception = True
+        
+        assert is_exception
+
+    @pytest.mark.tags("L1")
+    def test_cache_health_check(self):
+        """
+        target: test hit the cache function
+        method: keep default similarity_threshold
+        expected: hit successfully but return wrong reponse to user
+        """
+        if os.path.isfile("./sqlite.db"):
+            os.remove("./sqlite.db")
+        if os.path.isdir('./milvus_data'):
+            shutil.rmtree('./milvus_data')
+
+        onnx = SBERT("/home/bryan/Projects/Github/PayPal/ModelZoo/transformers/all-MiniLM-L6-v2")
+        vector_base = VectorBase("milvus", dimension=onnx.dimension, local_mode=True)
+        data_manager = get_data_manager("sqlite", vector_base, max_size=2000)
+        cache.init(
+            embedding_func=onnx.to_embeddings,
+            data_manager=data_manager,
+            similarity_evaluation=SearchDistanceEvaluation(
+                vectordb=vector_base,
+                cache_check=True,
+            ),
+            config=Config(
+                log_time_func=cf.log_time_func,
+                enable_token_counter=False,
+            ),
+        )
+
+        question = [
+            "what is apple?",
+            "what is intel?",
+            "what is openai?",
+
+        ]
+        answer = [
+            "apple",
+            "intel",
+            "openai"
+        ]
+        for q, a in zip(question, answer):
+            cache.data_manager.save(q, a, cache.embedding_func(q))
+
+        # let's simulate cache out-of-sync
+        # situation.
+        touble_query = "what is google?"
+        cache.data_manager.v.update_embeddings(1, cache.embedding_func(touble_query))
+
+        # cache health enabled
+        # stop returning incorrect answer
+        # and self-heal the trouble cache
+        # entry.
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": touble_query},
+            ],
+            search_only=True,
+            stream=True,
+        )
+        assert response is None
+        
+        # disable cache check, and verify
+        # cache is now consistent
+        cache.similarity_evaluation.cache_check = False
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": touble_query},
+            ],
+            search_only=True,
+            stream=True,
+        )
+        assert response is None
+
+        # verify self-heal took place
+        cache.similarity_evaluation.cache_check = False
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": question[0]},
+            ],
+            search_only=True,
+            stream=True,
+        )
+        assert get_text_response(response) == answer[0]
+
+       
+
+
+
+a = TestSqliteMilvus()
+a.test_cache_health_check()
