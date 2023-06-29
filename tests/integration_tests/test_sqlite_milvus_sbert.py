4e9f1fef22e8dee7c655f9e3a850ad6ea493a409
Bryan <wybryan@hotmail.com>
2023-06-29 11:53:09 +0800

    fix annoying milvus start problem in UT

--- tests/integration_tests/test_sqlite_milvus_sbert.py
+++ tests/integration_tests/test_sqlite_milvus_sbert.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 import pytest
-
+from tempfile import TemporaryDirectory
 from base.client_base import Base
 from common import common_func as cf
 from gptcache import cache, Config
@@ -36,96 +36,104 @@ class TestSqliteMilvus(Base):
         method: keep default similarity_threshold
         expected: cache health detection & correction
         """
-        if os.path.isfile("./sqlite.db"):
-            os.remove("./sqlite.db")
-        if os.path.isdir('./milvus_data'):
-            shutil.rmtree('./milvus_data')
+        with TemporaryDirectory(dir="./") as root:
+            if os.path.isfile("./sqlite.db"):
+                os.remove("./sqlite.db")
+            if os.path.isdir('./milvus_data'):
+                shutil.rmtree('./milvus_data')
 
-        onnx = SBERT()
-        vector_base = VectorBase("milvus", dimension=onnx.dimension, local_mode=True)
-        data_manager = get_data_manager("sqlite", vector_base, max_size=2000)
-        cache.init(
-            embedding_func=onnx.to_embeddings,
-            data_manager=data_manager,
-            similarity_evaluation=SearchDistanceEvaluation(),
-            config=Config(
-                log_time_func=cf.log_time_func,
-                enable_token_counter=False,
-            ),
-        )
+            onnx = SBERT()
+            vector_base = VectorBase(
+                "milvus",
+                dimension=onnx.dimension,
+                local_mode=True,
+                local_data=str(root),
+            )
+            data_manager = get_data_manager("sqlite", vector_base, max_size=2000)
+            cache.init(
+                embedding_func=onnx.to_embeddings,
+                data_manager=data_manager,
+                similarity_evaluation=SearchDistanceEvaluation(),
+                config=Config(
+                    log_time_func=cf.log_time_func,
+                    enable_token_counter=False,
+                ),
+            )
 
-        question = [
-            "what is apple?",
-            "what is intel?",
-            "what is openai?",
+            question = [
+                "what is apple?",
+                "what is intel?",
+                "what is openai?",
 
-        ]
-        answer = [
-            "apple",
-            "intel",
-            "openai"
-        ]
-        for q, a in zip(question, answer):
-            cache.data_manager.save(q, a, cache.embedding_func(q))
+            ]
+            answer = [
+                "apple",
+                "intel",
+                "openai"
+            ]
+            for q, a in zip(question, answer):
+                cache.data_manager.save(q, a, cache.embedding_func(q))
 
-        # let's simulate cache out-of-sync
-        # situation.
-        touble_query = "what is google?"
-        cache.data_manager.v.update_embeddings(1, cache.embedding_func(touble_query))
+            # let's simulate cache out-of-sync
+            # situation.
+            touble_query = "what is google?"
+            cache.data_manager.v.update_embeddings(1, cache.embedding_func(touble_query))
 
-        # without cache health check
-        # respons is incorrect
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": touble_query},
-            ],
-            search_only=True,
-            stream=True,
-        )
-        # Incorrect response "apple" returned to user
-        resp_txt = get_text_response(response)
-        # log.info(f"Inccorect response = {resp_txt} is returned")
-        assert answer[0] == resp_txt
+            # without cache health check
+            # respons is incorrect
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "user", "content": touble_query},
+                ],
+                search_only=True,
+                stream=True,
+            )
+            # Incorrect response "apple" returned to user
+            resp_txt = get_text_response(response)
+            # log.info(f"Inccorect response = {resp_txt} is returned")
+            assert answer[0] == resp_txt
 
-        # cache health enabled
-        # stop returning incorrect answer
-        # and self-heal the trouble cache
-        # entry.
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": touble_query},
-            ],
-            search_only=True,
-            health_check=True,
-            stream=True,
-        )
-        assert response is None
-        
-        # disable cache check, and verify
-        # cache is now consistent
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": touble_query},
-            ],
-            search_only=True,
-            stream=True,
-        )
-        assert response is None
+            # cache health enabled
+            # stop returning incorrect answer
+            # and self-heal the trouble cache
+            # entry.
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "user", "content": touble_query},
+                ],
+                search_only=True,
+                health_check=True,
+                stream=True,
+            )
+            assert response is None
+            
+            # disable cache check, and verify
+            # cache is now consistent
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "user", "content": touble_query},
+                ],
+                search_only=True,
+                stream=True,
+            )
+            assert response is None
 
-        # verify self-heal took place
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": question[0]},
-            ],
-            search_only=True,
-            stream=True,
-        )
-        assert get_text_response(response) == answer[0]
+            # verify self-heal took place
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "user", "content": question[0]},
+                ],
+                search_only=True,
+                stream=True,
+            )
+            assert get_text_response(response) == answer[0]
+            vector_base.rebuild()
+            vector_base.close()
