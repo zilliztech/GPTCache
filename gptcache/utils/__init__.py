94c8471ab973c7af616e2f7312d27323ea41e7e9
Parth Patel <41171860+parthvnp@users.noreply.github.com>
2023-06-29 03:08:21 -0400

    Add support for Qdrant Vector Store (#453)
    * Add Qdrant vector store client
    
    * Add setup for Qdrant vector store
    
    * Add lazy import for Qdrant client library
    
    * Add import_qdrant to index file
    
    * Use models not types for building configs
    
    * Add tests for Qdrant vector store

--- gptcache/utils/__init__.py
+++ gptcache/utils/__init__.py
@@ -38,7 +38,8 @@ __all__ = [
     "import_paddlenlp",
     "import_tiktoken",
     "import_fastapi",
-    "import_redis"
+    "import_redis",
+    "import_qdrant"
     ]
 
 import importlib.util
@@ -63,6 +64,10 @@ def import_pymilvus():
 
 def import_milvus_lite():
     _check_library("milvus")
+
+
+def import_qdrant():
+    _check_library("qdrant_client")
 
 
 def import_sbert():
