3bde5c450f263b491f053ef717f52ba4c6012134
SimFG <bang.fu@zilliz.com>
2023-06-29 17:01:12 +0800

    Fix the pylint error and add the chromedb test (#457)
    Signed-off-by: SimFG <bang.fu@zilliz.com>
    Signed-off-by: sunilkumardash9 <sunilkumardash9@gmail.com>

--- gptcache/manager/vector_data/qdrant.py
+++ gptcache/manager/vector_data/qdrant.py
@@ -1,36 +1,44 @@
 from typing import List, Optional
+
 import numpy as np
 
+from gptcache.manager.vector_data.base import VectorBase, VectorData
 from gptcache.utils import import_qdrant
 from gptcache.utils.log import gptcache_log
-from gptcache.manager.vector_data.base import VectorBase, VectorData
 
 import_qdrant()
 
-from qdrant_client import QdrantClient  # pylint: disable=C0413
-from qdrant_client.models import PointStruct, HnswConfigDiff, VectorParams, OptimizersConfigDiff, \
-    Distance  # pylint: disable=C0413
+# pylint: disable=C0413
+from qdrant_client import QdrantClient
+from qdrant_client.models import (
+    PointStruct,
+    HnswConfigDiff,
+    VectorParams,
+    OptimizersConfigDiff,
+    Distance,
+)
 
 
 class QdrantVectorStore(VectorBase):
+    """Qdrant Vector Store"""
 
     def __init__(
-            self,
-            url: Optional[str] = None,
-            port: Optional[int] = 6333,
-            grpc_port: int = 6334,
-            prefer_grpc: bool = False,
-            https: Optional[bool] = None,
-            api_key: Optional[str] = None,
-            prefix: Optional[str] = None,
-            timeout: Optional[float] = None,
-            host: Optional[str] = None,
-            collection_name: Optional[str] = "gptcache",
-            location: Optional[str] = "./qdrant",
-            dimension: int = 0,
-            top_k: int = 1,
-            flush_interval_sec: int = 5,
-            index_params: Optional[dict] = None,
+        self,
+        url: Optional[str] = None,
+        port: Optional[int] = 6333,
+        grpc_port: int = 6334,
+        prefer_grpc: bool = False,
+        https: Optional[bool] = None,
+        api_key: Optional[str] = None,
+        prefix: Optional[str] = None,
+        timeout: Optional[float] = None,
+        host: Optional[str] = None,
+        collection_name: Optional[str] = "gptcache",
+        location: Optional[str] = "./qdrant",
+        dimension: int = 0,
+        top_k: int = 1,
+        flush_interval_sec: int = 5,
+        index_params: Optional[dict] = None,
     ):
         if dimension <= 0:
             raise ValueError(
@@ -44,13 +52,17 @@ class QdrantVectorStore(VectorBase):
         if self._in_memory or location is not None:
             self._create_local(location)
         else:
-            self._create_remote(url, port, api_key, timeout, host, grpc_port, prefer_grpc, prefix, https)
+            self._create_remote(
+                url, port, api_key, timeout, host, grpc_port, prefer_grpc, prefix, https
+            )
         self._create_collection(collection_name, flush_interval_sec, index_params)
 
     def _create_local(self, location):
         self._client = QdrantClient(location=location)
 
-    def _create_remote(self, url, port, api_key, timeout, host, grpc_port, prefer_grpc, prefix, https):
+    def _create_remote(
+        self, url, port, api_key, timeout, host, grpc_port, prefer_grpc, prefix, https
+    ):
         self._client = QdrantClient(
             url=url,
             port=port,
@@ -63,45 +75,70 @@ class QdrantVectorStore(VectorBase):
             https=https,
         )
 
-    def _create_collection(self, collection_name: str, flush_interval_sec: int, index_params: Optional[dict] = None):
+    def _create_collection(
+        self,
+        collection_name: str,
+        flush_interval_sec: int,
+        index_params: Optional[dict] = None,
+    ):
         hnsw_config = HnswConfigDiff(**(index_params or {}))
-        vectors_config = VectorParams(size=self.dimension, distance=Distance.COSINE,
-                                      hnsw_config=hnsw_config)
-        optimizers_config = OptimizersConfigDiff(deleted_threshold=0.2, vacuum_min_vector_number=1000,
-                                                 flush_interval_sec=flush_interval_sec)
+        vectors_config = VectorParams(
+            size=self.dimension, distance=Distance.COSINE, hnsw_config=hnsw_config
+        )
+        optimizers_config = OptimizersConfigDiff(
+            deleted_threshold=0.2,
+            vacuum_min_vector_number=1000,
+            flush_interval_sec=flush_interval_sec,
+        )
         # check if the collection exists
         existing_collections = self._client.get_collections()
         for existing_collection in existing_collections.collections:
             if existing_collection.name == collection_name:
-                gptcache_log.warning("The %s collection already exists, and it will be used directly.", collection_name)
+                gptcache_log.warning(
+                    "The %s collection already exists, and it will be used directly.",
+                    collection_name,
+                )
                 break
         else:
-            self._client.create_collection(collection_name=collection_name, vectors_config=vectors_config,
-                                           optimizers_config=optimizers_config)
+            self._client.create_collection(
+                collection_name=collection_name,
+                vectors_config=vectors_config,
+                optimizers_config=optimizers_config,
+            )
 
     def mul_add(self, datas: List[VectorData]):
-        points = [PointStruct(id=d.id, vector=d.data.reshape(-1).tolist()) for d in datas]
-        self._client.upsert(collection_name=self._collection_name, points=points, wait=False)
+        points = [
+            PointStruct(id=d.id, vector=d.data.reshape(-1).tolist()) for d in datas
+        ]
+        self._client.upsert(
+            collection_name=self._collection_name, points=points, wait=False
+        )
 
     def search(self, data: np.ndarray, top_k: int = -1):
         if top_k == -1:
             top_k = self.top_k
         reshaped_data = data.reshape(-1).tolist()
-        search_result = self._client.search(collection_name=self._collection_name, query_vector=reshaped_data,
-                                            limit=top_k)
+        search_result = self._client.search(
+            collection_name=self._collection_name,
+            query_vector=reshaped_data,
+            limit=top_k,
+        )
         return list(map(lambda x: (x.score, x.id), search_result))
 
     def delete(self, ids: List[str]):
         self._client.delete(collection_name=self._collection_name, points_selector=ids)
 
     def rebuild(self, ids=None):  # pylint: disable=unused-argument
-        optimizers_config = OptimizersConfigDiff(deleted_threshold=0.2, vacuum_min_vector_number=1000)
-        self._client.update_collection(collection_name=self._collection_name, optimizer_config=optimizers_config)
+        optimizers_config = OptimizersConfigDiff(
+            deleted_threshold=0.2, vacuum_min_vector_number=1000
+        )
+        self._client.update_collection(
+            collection_name=self._collection_name, optimizer_config=optimizers_config
+        )
 
     def flush(self):
         # no need to flush manually as qdrant flushes automatically based on the optimizers_config for remote Qdrant
         pass
-
 
     def close(self):
         self.flush()
--- tests/integration_tests/test_sqlite_milvus_sbert.py
+++ tests/integration_tests/test_sqlite_milvus_sbert.py
@@ -1,15 +1,16 @@
 import os
 import shutil
-import pytest
 from tempfile import TemporaryDirectory
+
+import pytest
+
 from base.client_base import Base
 from common import common_func as cf
 from gptcache import cache, Config
 from gptcache.adapter import openai
-from gptcache.embedding import Onnx, SBERT
+from gptcache.embedding import SBERT
 from gptcache.manager import get_data_manager, VectorBase
 from gptcache.similarity_evaluation.distance import SearchDistanceEvaluation
-from utils.util_log import test_log as log
 
 
 def get_text_response(response):
@@ -37,104 +38,112 @@ class TestSqliteMilvus(Base):
         expected: cache health detection & correction
         """
         with TemporaryDirectory(dir="./") as root:
-            if os.path.isfile("./sqlite.db"):
-                os.remove("./sqlite.db")
-            if os.path.isdir('./milvus_data'):
-                shutil.rmtree('./milvus_data')
 
             onnx = SBERT()
-            vector_base = VectorBase(
-                "milvus",
-                dimension=onnx.dimension,
-                local_mode=True,
-                port="10086",
-                local_data=str(root),
-            )
-            data_manager = get_data_manager("sqlite", vector_base, max_size=2000)
-            cache.init(
-                embedding_func=onnx.to_embeddings,
-                data_manager=data_manager,
-                similarity_evaluation=SearchDistanceEvaluation(),
-                config=Config(
-                    log_time_func=cf.log_time_func,
-                    enable_token_counter=False,
+
+            vector_bases = [
+                VectorBase(
+                    "milvus",
+                    dimension=onnx.dimension,
+                    local_mode=True,
+                    port="10086",
+                    local_data=str(root),
                 ),
-            )
-
-            question = [
-                "what is apple?",
-                "what is intel?",
-                "what is openai?",
-
+                VectorBase("chromadb"),
             ]
-            answer = [
-                "apple",
-                "intel",
-                "openai"
-            ]
-            for q, a in zip(question, answer):
-                cache.data_manager.save(q, a, cache.embedding_func(q))
 
-            # let's simulate cache out-of-sync
-            # situation.
-            touble_query = "what is google?"
-            cache.data_manager.v.update_embeddings(1, cache.embedding_func(touble_query))
+            for vector_base in vector_bases:
+                if os.path.isfile("./sqlite.db"):
+                    os.remove("./sqlite.db")
+                if os.path.isdir('./milvus_data'):
+                    shutil.rmtree('./milvus_data')
 
-            # without cache health check
-            # respons is incorrect
-            response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                    {"role": "system", "content": "You are a helpful assistant."},
-                    {"role": "user", "content": touble_query},
-                ],
-                search_only=True,
-                stream=True,
-            )
-            # Incorrect response "apple" returned to user
-            resp_txt = get_text_response(response)
-            # log.info(f"Inccorect response = {resp_txt} is returned")
-            assert answer[0] == resp_txt
+                data_manager = get_data_manager("sqlite", vector_base, max_size=2000)
+                cache.init(
+                    embedding_func=onnx.to_embeddings,
+                    data_manager=data_manager,
+                    similarity_evaluation=SearchDistanceEvaluation(),
+                    config=Config(
+                        log_time_func=cf.log_time_func,
+                        enable_token_counter=False,
+                    ),
+                )
 
-            # cache health enabled
-            # stop returning incorrect answer
-            # and self-heal the trouble cache
-            # entry.
-            response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                    {"role": "system", "content": "You are a helpful assistant."},
-                    {"role": "user", "content": touble_query},
-                ],
-                search_only=True,
-                health_check=True,
-                stream=True,
-            )
-            assert response is None
-            
-            # disable cache check, and verify
-            # cache is now consistent
-            response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                    {"role": "system", "content": "You are a helpful assistant."},
-                    {"role": "user", "content": touble_query},
-                ],
-                search_only=True,
-                stream=True,
-            )
-            assert response is None
+                question = [
+                    "what is apple?",
+                    "what is intel?",
+                    "what is openai?",
 
-            # verify self-heal took place
-            response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                    {"role": "system", "content": "You are a helpful assistant."},
-                    {"role": "user", "content": question[0]},
-                ],
-                search_only=True,
-                stream=True,
-            )
-            assert get_text_response(response) == answer[0]
-            if os.path.isfile("./sqlite.db"):
-                os.remove("./sqlite.db")
+                ]
+                answer = [
+                    "apple",
+                    "intel",
+                    "openai"
+                ]
+                for q, a in zip(question, answer):
+                    cache.data_manager.save(q, a, cache.embedding_func(q))
+
+                # let's simulate cache out-of-sync
+                # situation.
+                touble_query = "what is google?"
+                cache.data_manager.v.update_embeddings(1, cache.embedding_func(touble_query))
+
+                # without cache health check
+                # respons is incorrect
+                response = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    messages=[
+                        {"role": "system", "content": "You are a helpful assistant."},
+                        {"role": "user", "content": touble_query},
+                    ],
+                    search_only=True,
+                    stream=True,
+                )
+                # Incorrect response "apple" returned to user
+                resp_txt = get_text_response(response)
+                # log.info(f"Inccorect response = {resp_txt} is returned")
+                assert answer[0] == resp_txt
+
+                # cache health enabled
+                # stop returning incorrect answer
+                # and self-heal the trouble cache
+                # entry.
+                cache.config.data_check = True
+                response = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    messages=[
+                        {"role": "system", "content": "You are a helpful assistant."},
+                        {"role": "user", "content": touble_query},
+                    ],
+                    search_only=True,
+                    stream=True,
+                )
+                assert response is None
+
+                # disable cache check, and verify
+                # cache is now consistent
+                cache.config.data_check = False
+                response = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    messages=[
+                        {"role": "system", "content": "You are a helpful assistant."},
+                        {"role": "user", "content": touble_query},
+                    ],
+                    search_only=True,
+                    stream=True,
+                )
+                assert response is None
+
+                # verify self-heal took place
+                response = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    messages=[
+                        {"role": "system", "content": "You are a helpful assistant."},
+                        {"role": "user", "content": question[0]},
+                    ],
+                    search_only=True,
+                    stream=True,
+                )
+                assert get_text_response(response) == answer[0]
+                if os.path.isfile("./sqlite.db"):
+                    os.remove("./sqlite.db")
