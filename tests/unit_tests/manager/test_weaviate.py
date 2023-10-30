6e7789e20394cd36bd2ef5a23112ad91b7a45358
Amrit Singh <amrit2@cisco.com>
2023-10-30 08:41:39 -0400

    Update test_weaviate.py
    Update unit test
    
    Signed-off-by: Amrit Singh <amrit2@cisco.com>

--- tests/unit_tests/manager/test_weaviate.py
+++ tests/unit_tests/manager/test_weaviate.py
@@ -13,7 +13,7 @@ class TestWeaviateDB(unittest.TestCase):
         class_name = "Vectorcache"
 
         db = VectorBase(
-            "weaviate",
+            "weaviate-client",
             class_name=class_name,
             top_k=top_k
         )
@@ -53,7 +53,7 @@ class TestWeaviateDB(unittest.TestCase):
         }
 
         db = VectorBase(
-            "weaviate",
+            "weaviate-client",
             class_schema=class_schema,
             top_k=top_k
         )
